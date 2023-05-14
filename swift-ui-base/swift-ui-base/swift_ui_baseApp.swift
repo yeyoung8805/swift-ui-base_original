4fa2abec3c1adc9519e9dc10487ea688c93d9f8f
“yeyoung8805” <yyk8805@naver.com>
2023-05-15 04:16:53 +0900

    project init

---
+++ swift-ui-base/swift-ui-base/swift_ui_baseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_baseApp.swift
+//  swift-ui-base
+//
+//  Created by 이진솔 on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_baseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
